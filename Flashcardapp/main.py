21bd4d689d5a933d12198cda8ec33782b3217eba
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-04-30 23:47:38 +0100

    psudo code done for flashcard app

--- Flashcardapp/main.py
+++ Flashcardapp/main.py
@@ -6,3 +6,41 @@
 # user need to be able to add , remove and edit subjects or questions 
 # need to be able to randomly quizz the user
 # must use OOP for the project 
+
+# psudo code 
+
+# object adding : get the topic and make a collenct the questions
+    # get the input and make a new key:value pair
+
+# object app :
+    # all the functions in the main code goes here 
+
+# secutity:
+    # number error trapping 
+    # stores the username and password 
+
+# main loop :
+    # choices 
+    # start a new topic 
+        # add the topic and then questions
+        # save it to the dict 
+    # see questions
+        # give out the topics and then show the questions based on the input
+    # random Questions 
+        # select a topic or enter 5 to choose a random one
+    # more options 
+        # asks for a username and a password 
+        # if the pw and username is correct 
+            # delete questions
+                # ask for the topic and then delete the questions based on the input 
+                # display all the questions uder that one topic 
+                # let the user decide which question he wants to delete 5 for all
+            # delete topics 
+                # delete the entire key:value pair 
+            
+
+    
+
+# flashcard = {"Science":["what is an atom",
+#                         "Define life"]}   # Example of how the topic and questions should be 
+
