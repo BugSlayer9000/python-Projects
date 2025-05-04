0b2265e332025eabe3c16c4b839c9d441d5de259
samod <83113845+BugSlayer9000@users.noreply.github.com>
2025-05-05 00:57:05 +0100

    Choice 4 finished ✅👌

--- Flashcardapp/main.py
+++ Flashcardapp/main.py
@@ -84,8 +84,43 @@ class FlashCard:
             
         
         return ranqizesV2
- 
-
+    
+    def delTopic(self,index):
+        # get the topic under the index 
+        # delete the key and value under the index 
+        # return a sucessful massege
+        
+        del self.flashcardDic[index]
+        
+        return f"Topic `{index}` deleted succesfully."
+        
+class LoginData:
+    def __init__(self):
+        self.userData = {"samod":"samod1"}
+        self.attempts = 0
+        self.login = False
+        
+    def loginCheck(self,username,password):
+        
+        if self.attempts <= 3:
+            if username in self.userData:
+                if password in self.userData[username]: 
+                    self.login = True
+                    return f"Access sucessful !",self.attempts,self.login
+                else:
+                    self.attempts += 1
+                    return f"Wrong Password !", self.attempts, self.login
+            else:
+                self.attempts += 1
+                return f"Wrong Username and password !", self.attempts, self.login
+                
+        else:
+            return f"You have no attempts left", self.attempts, self.login
+    
+    def addNewLogin(self, newUser, newPw):
+        self.userData[newUser] = newPw
+        return f"New User added \nUsername - {newUser} \nPassword - {newPw}"
+           
 class Security:
     def __init__(self):
         # self.password = password    # password and the username is for later options 
@@ -104,6 +139,8 @@ def main():
     
     lock = Security() 
     
+    login = LoginData()
+    
     is_running = True
     
     questionlist = []
@@ -116,7 +153,7 @@ def main():
         print("1 -- Start a new topic")
         print("2 -- Look at questions")     # 2 sub choices 
         print("3 -- Random questions")      # 2 sub choices 
-        print("4 -- More Options")          # whole another choices 
+        print("4 -- Advanced Options")          # whole another choices 
         print("5 -- Quit")          
         
         
@@ -229,8 +266,113 @@ def main():
             for i,quiz in enumerate(ranquiz,1):
                 print(f"{i}. {quiz}")
                 
-        elif checked_Num == 4:
-            pass
+        elif checked_Num == 4: # Advanced options 
+            # options 
+                # 1.Login
+                    # delete 
+                        # topic or a question under a topic
+                # 2.Register 
+                    # add a password and a user 
+             
+            print("\n-------------------Advanced Options-------------")
+                
+            print("\n1.Login")
+            print("2.Register as a new user")
+                
+            adChoice = input("\nEnter your option : ")
+            
+            checkedAdchoice = lock.numcheck(adChoice)
+            
+            if checkedAdchoice >= 3:
+                print("Choice not valid! ")
+            
+            if checkedAdchoice == 1: # login
+                while login.attempts != 4:
+                    username = input("Enter your username : ") # ask for the username
+                    
+                    if username == "":
+                        print("\nYou must enter a username !") # Empty input check 
+                        continue
+                    
+                    password = input("Enter your password : ")
+                    
+                    if password == "":
+                        print("\nYou must enter a password !") # Empty input check 
+                        continue
+                    
+                    response, attempts, loginCheck = login.loginCheck(username,password) # pass the username and password to the login data and get back the validation as a tuple
+                    
+                    # response = Access sucessful ! , attempts < 3 , loginCheck = True
+                        # ask the user delete topic or questions 
+                            # show topics
+                            # let the user choose one 
+                            # choice topic 
+                                # delete the topic 
+                            # choice questions
+                                # show the questions and let the user choose one
+                    
+                    if loginCheck == True:
+                        print("\n1.Topics")
+                        print("2.Questions")
+                        
+                        delChoice = input("\nDo want to delete or topics : ")
+                        
+                        checkeddelChoice = lock.numcheck(delChoice)
+                        
+                        if checkeddelChoice > 3:
+                            print("Enter a Valid Input !")
+                            continue
+                        
+                        print()
+                        
+                        if checkeddelChoice == 1: # show the topics 
+                            
+                            delTopicList = [] # topics from the dic
+                            
+                            for i, topics in enumerate(flashcard.flashcardDic.keys(),1):
+                                delTopicList.append(topics)
+                                print(f"{i}.{topics}")
+                            
+                            delTopicChoice = input("Enter the number of topic you want to delete : ")
+
+                            CheckeddelTopicChoice = lock.numcheck(delTopicChoice)
+                            
+                            if CheckeddelTopicChoice > len(delTopicList): # input check
+                                print("Invalid input")
+                                continue
+                            
+                            choosenDelTopic = delTopicList[CheckeddelTopicChoice-1]
+                            
+                            result = flashcard.delTopic(choosenDelTopic)
+                            
+                            print(result)
+                            break
+                        elif checkeddelChoice == 2:
+                            # add the delete question thing 
+                            pass
+                            
+                            
+            if checkedAdchoice == 2:
+                # get the new username
+                # get the new password 
+                # add them to the login database 
+                
+                newUser = input("\nEnter the new user name to add : ")
+                
+                if newUser == "":
+                    print("No username Detected !")
+                    continue
+                
+                newPw = input(f"Enter the password for {newUser} : ")
+                
+                if newPw == "":
+                    print("Empty password detected ! ")
+                    continue
+                
+                newLogin = login.addNewLogin(newUser,newPw)
+                
+                print(newLogin)
+                  
         elif checked_Num == 5:
             print(f"Goodbye ! ")
             break
